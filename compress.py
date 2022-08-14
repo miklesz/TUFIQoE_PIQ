8ea9bcfda7a7043d87f847778c5025d253b290d9
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-08-15 00:20:45 +0200

    After VMAF, I also made some extra VIF

--- compress.py
+++ compress.py
@@ -1,50 +1,86 @@
 import os
-import pickle
+# import pickle
+import pyiqa
+import sys
+import torchvision.transforms as transforms
 from PIL import Image
 src = 'Lenna_(test_image).png'
+
+# metric = 'vmaf'
+# hrc = {
+#     'A': None,
+#     'B': 80,
+#     'C': 60,
+#     'D': 40,
+#     'E': 20,
+# }
+
+metric = 'vif'
 hrc = {
     'A': None,
-    'B': 80,
-    'C': 60,
-    'D': 40,
-    'E': 20,
+    'B': .80,
+    'C': .60,
+    'D': .40,
+    'E': .20,
 }
-metric = 'vmaf'
+
 src_no_ext = src[:src.find('.')]
 
-# # Main compress and metric loop
-# for f in os.listdir('tmp'):
-#     os.remove(os.path.join('tmp', f))
-# image = Image.open(src)
-# os.system(f'ffmpeg -y -i "{src}" -pix_fmt yuv444p tmp/reference.y4m')
-# metric_values = []
-# for q in range(101):
-#     pvs = os.path.join('tmp', f'{src_no_ext}_{q}.jpg')
-#     image.save(pvs, 'JPEG', quality=q)
-#     os.system(f'ffmpeg -y -i "{pvs}" -pix_fmt yuv444p tmp/distorted.y4m')
-#     os.system('vmaf -r tmp/reference.y4m -d tmp/distorted.y4m -o tmp/output.csv --csv --threads 8')
-#     with open('tmp/output.csv') as vmaf_file:
-#         metric_values.append(float(vmaf_file.read().split(',')[-2]))
-# with open('metric_values', 'wb') as metric_values_file:
-#     pickle.dump(metric_values, metric_values_file)
+# Create VIF metric with default setting
+vif_metric = pyiqa.create_metric('vif')
 
-with open('metric_values', 'rb') as metric_values_file:
-    metric_values = pickle.load(metric_values_file)
+to_tensor = transforms.ToTensor()
+# sys.exit()
+
+# Main compress and metric loop
+for f in os.listdir('tmp'):
+    os.remove(os.path.join('tmp', f))
+metric_values = []
+image = Image.open(src)
+
+if metric == 'vmaf':
+    os.system(f'ffmpeg -y -i "{src}" -pix_fmt yuv444p tmp/reference.y4m')
+    for q in range(101):
+        pvs = os.path.join('tmp', f'{src_no_ext}_{q}.jpg')
+        image.save(pvs, 'JPEG', quality=q)
+        os.system(f'ffmpeg -y -i "{pvs}" -pix_fmt yuv444p tmp/distorted.y4m')
+        os.system('vmaf -r tmp/reference.y4m -d tmp/distorted.y4m -o tmp/output.csv --csv --feature vif --threads 8')
+        with open('tmp/output.csv') as vmaf_file:
+            metric_values.append(float(vmaf_file.read().split(',')[-2]))
+    # with open('metric_values', 'wb') as metric_values_file:
+    #     pickle.dump(metric_values, metric_values_file)
+    # with open('metric_values', 'rb') as metric_values_file:
+    #     metric_values = pickle.load(metric_values_file)
+elif metric == 'vif':
+    r = Image.open(src).convert('RGB')
+    r_tensor = to_tensor(r)
+    r_tensor = r_tensor.unsqueeze(0)
+    for q in range(101):
+        pvs = os.path.join('tmp', f'{src_no_ext}_{q}.jpg')
+        image.save(pvs, 'JPEG', quality=q)
+        d = Image.open(pvs).convert('RGB')
+        d_tensor = to_tensor(d)
+        d_tensor = d_tensor.unsqueeze(0)
+        score_tensor = vif_metric(r_tensor, d_tensor)
+        metric_values.append(score_tensor.item())
+
 print(metric_values)
+# sys.exit()
+
 for hrc_key in hrc:
-    print(hrc_key)
     desired_metric = hrc[hrc_key]
-    print(desired_metric)
     if desired_metric is None:
         os.system(f'cp "{src}" "{src_no_ext}_{hrc_key}.jpg"')
+        print(f'hrc_key = {hrc_key}, desired_metric = {desired_metric}')
     else:
         distance = abs(desired_metric-metric_values[0])
-        distance_i = 0
+        distance_index = 0
         for metric_i in range(1, 101):
             if abs(desired_metric-metric_values[metric_i]) < distance:
                 distance = abs(desired_metric - metric_values[metric_i])
-                distance_i = metric_i
-        pvs_file = os.path.join('tmp', f'{src_no_ext}_{distance_i}.jpg')
+                distance_index = metric_i
+        pvs_file = os.path.join('tmp', f'{src_no_ext}_{distance_index}.jpg')
         os.system(f'cp "{pvs_file}" "{src_no_ext}_{hrc_key}.jpg"')
-        print(distance_i, distance)
-
+        print(f'hrc_key = {hrc_key}, desired_metric = {desired_metric}, '
+              f'distance_index = {distance_index}, distance = {distance}')
+sys.exit()
