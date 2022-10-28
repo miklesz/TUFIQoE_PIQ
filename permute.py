38ec83dc2eb7f5a11e8e18f0002fedffca4daac5
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-29 00:05:19 +0200

    Update permute.py

--- permute.py
+++ permute.py
@@ -1,8 +1,22 @@
 # Standard library imports
+import random
 import requests
 
 
+# Constants
 URL = 'http://pbz.kt.agh.edu.pl/~testySubiektywne/PIQMOS/config/src.csv'
-src = [line.split(',')[0] for line in str(requests.get(URL).content).split('\\n')[1:-1]]
-print(src)
-print(type(src))
+ORDERS = 5
+LEVELS = ('A', 'B', 'C', 'D', 'E')
+
+srcs = [line.split(',')[0] for line in str(requests.get(URL).content).split('\\n')[1:-1]]
+print(srcs)
+print(type(srcs))
+order_id = 0
+with open('orders.csv', 'w') as file:
+    file.write(f'order_id,{",".join(srcs)}\n')
+    for five_id in range(ORDERS//5+1):
+        versions = [random.sample(LEVELS, len(LEVELS)) for src in srcs]
+        for in_five_id in range(5):
+            print(versions)
+            # file.write(f'{order_id},\n')
+            order_id += 1
