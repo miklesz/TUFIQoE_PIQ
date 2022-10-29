5a0a4c9fd169f5a04f86f068352eacedc200c06d
Mikołaj Leszczuk <leszczuk@agh.edu.pl>
2022-10-29 16:54:41 +0200

    Update permute.py

--- permute.py
+++ permute.py
@@ -5,18 +5,25 @@ import requests
 
 # Constants
 URL = 'http://pbz.kt.agh.edu.pl/~testySubiektywne/PIQMOS/config/src.csv'
-ORDERS = 5
+SERIES = 1
 LEVELS = ('A', 'B', 'C', 'D', 'E')
 
+
+# Functions
+def print_write(file_object, line):
+    print(line)
+    file_object.write(f'{line}\n')
+
+
 srcs = [line.split(',')[0] for line in str(requests.get(URL).content).split('\\n')[1:-1]]
-print(srcs)
-print(type(srcs))
-order_id = 0
+no = 0
 with open('orders.csv', 'w') as file:
-    file.write(f'order_id,{",".join(srcs)}\n')
-    for five_id in range(ORDERS//5+1):
+    print_write(file, f'no,{",".join(srcs)}')
+    for five_id in range(SERIES):
         versions = [random.sample(LEVELS, len(LEVELS)) for src in srcs]
         for in_five_id in range(5):
-            print(versions)
-            # file.write(f'{order_id},\n')
-            order_id += 1
+            print_write(
+                file,
+                f'{no},{",".join([srcs[i][:srcs[i].rfind(".")] + "_" + versions[i][in_five_id] + ".jpg" for i in range(len(srcs))])}'
+            )
+            no += 1
